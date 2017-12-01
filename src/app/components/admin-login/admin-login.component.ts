80e806aa9cdfff2611eea2032a787b96dbeee8be
IrtezaAsadRizvi <irteza.asad.cse@ulab.edu.bd>
2017-12-01 17:31:56 +0600

    finished login, saved info in localStorage, next: make logout method

--- src/app/components/admin-login/admin-login.component.ts
+++ src/app/components/admin-login/admin-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginModel } from '../../models/login.model';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-admin-login',
@@ -6,27 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-login.component.css']
 })
 export class AdminLoginComponent implements OnInit {
-  private isEmpty:boolean
-  constructor() { }
+  private reqObj:LoginModel;
+  private loginError:string;
+  private loginHint:string;
+
+  constructor(private api:ApiService) { }
 
   ngOnInit() {
+    this.reqObj = {
+      grant_type: "password",
+      client_id: "2",
+      client_secret: "kekuiHIVRFnjgk4fYCt8gmwGNL79KtIbVb8urumz",
+      username: "",
+      password: "",
+      scope: "*"
+    }
   }
 
   onSubmit(f){
-    if(f.form.value.username == '' || f.form.value.username == undefined){
-      this.isEmpty = true
-    }else{
-      this.isEmpty = false
-      console.log(f.form.value)
-    }
+    this.reqObj.username = f.form.value.email;
+    this.reqObj.password = f.form.value.password;
+    this.api.login(this.reqObj).subscribe((res)=>{
+      localStorage.setItem('currentUser', JSON.stringify(res));
+      console.log(JSON.parse(localStorage.getItem('currentUser')));
+    },(err)=>{
+      if(err.error.message == "The request is missing a required parameter, includes an invalid parameter value, includes a parameter more than once, or is otherwise malformed."){
+        this.loginError = "Please Fill in The Login informations first";
+      }else{
+        this.loginError = err.error.message;
+      }
+      this.loginHint = err.error.hint;
+    });
   }
 
   checkIfFilled(e){
-    if(e.target.value == ''){
-      this.isEmpty = true
-    }else{
-      this.isEmpty = false
-    }
+
   }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RegisterModel } from '../models/register.model';
+import { LoginModel } from '../models/login.model';
 
 @Injectable()
 export class ApiService {
-  private reqObj:RegisterModel;
+  private reqObj_reg:RegisterModel;
+  private reqObj_login:LoginModel;
 
   constructor(private http:HttpClient) { }
-
+  // user registration
   register(fromForm:RegisterModel){
-    this.reqObj = fromForm;
-    return this.http.post('http://127.0.0.1:8000/api/register', this.reqObj);
+    this.reqObj_reg = fromForm;
+    return this.http.post('http://127.0.0.1:8000/api/register', this.reqObj_reg);
+  }
+  // user login
+  login(fromForm:LoginModel){
+    this.reqObj_login = fromForm;
+    return this.http.post('http://127.0.0.1:8000/oauth/token', this.reqObj_login);
   }
 
 }
